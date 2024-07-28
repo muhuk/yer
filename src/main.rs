aa8bac1939fe23aeacc98bf901d35e12fb05107a
Atamert Ölçgen <muhuk@muhuk.com>
2024-07-28 18:21:15 +0300

    Change window title.
    Closes #3.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,17 @@ use bevy::prelude::*;
 
 mod ui;
 
+const WINDOW_TITLE: &str = "yer - Terrain Generation Toolkit";
+
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: WINDOW_TITLE.to_owned(),
+                ..Default::default()
+            }),
+            ..Default::default()
+        }))
         .add_plugins(ui::UiPlugin)
         .run();
 }
