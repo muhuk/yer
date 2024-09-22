7d0b66345dfab3088f5187c29887afcc93608b53
Atamert Ölçgen <muhuk@muhuk.com>
2024-09-22 22:42:38 +0300

    Add GPL license.

--- src/layer.rs
+++ src/layer.rs
@@ -1,3 +1,19 @@
+// Copyright © 2024 Atamert Ölçgen.
+// This file is part of Yer.
+//
+// Yer is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// Yer is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along
+// with Yer.  If not, see <https://www.gnu.org/licenses/>.
+
 use bevy::ecs::world::Command;
 use bevy::prelude::*;
 use bevy::time::common_conditions::on_timer;
