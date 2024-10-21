18fd854ed1431ecbd9582b31eab75f99950ea55e
Atamert Ölçgen <muhuk@muhuk.com>
2024-10-21 14:35:47 +0300

    Add yer suffix to selected filename if necessary.

--- src/ui/file_dialog.rs
+++ src/ui/file_dialog.rs
@@ -14,12 +14,17 @@
 // You should have received a copy of the GNU General Public License along
 // with Yer.  If not, see <https://www.gnu.org/licenses/>.
 
+use std::ffi::OsStr;
 use std::path::PathBuf;
 use std::sync::Arc;
 
 use bevy::prelude::*;
 use bevy_egui::egui::Context;
 use egui_file_dialog;
+
+static DEFAULT_FILE_NAME: &str = "untitled.yer";
+static PROJECT_FILES_FILTER_NAME: &str = "Project Files";
+static SUFFIX: &str = "yer";
 
 // PLUGIN
 
@@ -51,7 +56,9 @@ impl SaveFileDialog {
         match self.file_dialog.update(ctx).state() {
             egui_file_dialog::DialogState::Open => DialogState::Open,
             egui_file_dialog::DialogState::Cancelled => DialogState::Cancelled,
-            egui_file_dialog::DialogState::Selected(path) => DialogState::Selected(path),
+            egui_file_dialog::DialogState::Selected(path) => {
+                DialogState::Selected(sanitize_path(path))
+            }
             _ => unreachable!(),
         }
     }
@@ -59,14 +66,63 @@ impl SaveFileDialog {
 
 impl Default for SaveFileDialog {
     fn default() -> Self {
+        // `save_file` doesn't support file filters as of 0.6.1.
+        // See: https://github.com/fluxxcode/egui-file-dialog/issues/167
         let mut file_dialog = egui_file_dialog::FileDialog::new()
             .add_file_filter(
-                "Project files",
-                Arc::new(|path| path.extension().unwrap_or_default() == "yer"),
+                PROJECT_FILES_FILTER_NAME,
+                Arc::new(|path| path.extension().unwrap_or_default() == SUFFIX),
             )
+            .default_file_filter(PROJECT_FILES_FILTER_NAME)
+            .default_file_name(DEFAULT_FILE_NAME)
             .as_modal(true);
         file_dialog.save_file();
 
         Self { file_dialog }
+    }
+}
+
+fn sanitize_path(path: PathBuf) -> PathBuf {
+    let suffix = format!(".{}", SUFFIX);
+    if path.extension() != Some(OsStr::new(SUFFIX)) {
+        match path.file_name() {
+            Some(file_name) => {
+                let mut file_name = file_name.to_owned();
+                file_name.push(&suffix);
+                path.with_file_name(file_name)
+            }
+            None => panic!("sanitize_path is called with a PathBuf that doesn't have a file name."),
+        }
+    } else {
+        path
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sanitize_path_appends_extension_to_filename_without_extension() {
+        assert_eq!(
+            PathBuf::from("somefile.yer"),
+            sanitize_path(PathBuf::from("somefile"))
+        );
+    }
+
+    #[test]
+    fn sanitize_path_does_not_change_filename_with_correct_extension() {
+        assert_eq!(
+            PathBuf::from("somefile.yer"),
+            sanitize_path(PathBuf::from("somefile.yer"))
+        );
+    }
+
+    #[test]
+    fn sanitize_path_adds_extension_if_the_filename_has_a_different_extension() {
+        assert_eq!(
+            PathBuf::from("some.file.yer"),
+            sanitize_path(PathBuf::from("some.file"))
+        );
     }
 }
